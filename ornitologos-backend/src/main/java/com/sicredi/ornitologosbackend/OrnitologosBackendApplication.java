2afff125dce492758331ba0a13f66b83c3d7b1c1
kayque <kayque_silva@LT5M7W6L3.sicredi.net.br>
2022-11-01 17:21:44 -0300

    update

---
+++ ornitologos-backend/src/main/java/com/sicredi/ornitologosbackend/OrnitologosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sicredi.ornitologosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrnitologosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrnitologosBackendApplication.class, args);
+	}
+
+}
---
+++ ornitologos-backend/src/test/java/com/sicredi/ornitologosbackend/OrnitologosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicredi.ornitologosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrnitologosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
