48ffd06f2fb2666c66ecd2c467e2280ea6d06056
Anna Damasceno <anna_damasceno@sicredi.com.br>
2022-11-03 15:53:40 -0300

    Update AveRepository e AveService

--- ornitologos-backend/src/main/java/com/sicredi/ornitologosbackend/repositories/AveRepository.java
+++ ornitologos-backend/src/main/java/com/sicredi/ornitologosbackend/repositories/AveRepository.java
@@ -1,7 +1,15 @@
 package com.sicredi.ornitologosbackend.repositories;
 
 import com.sicredi.ornitologosbackend.entities.Ave;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AveRepository extends JpaRepository<Ave, Long> {
+    @Override
+    Ave save(Ave ave);
+
+    List<Ave> findByNomePtContainingOrNomeEnContainingOrNomeLtContainingOrCorOrHabitat(String nomePt, String nomeEn, String nomeLt, String cor, String habitat);
 }
--- ornitologos-backend/src/main/java/com/sicredi/ornitologosbackend/services/AveService.java
+++ ornitologos-backend/src/main/java/com/sicredi/ornitologosbackend/services/AveService.java
@@ -1,4 +1,24 @@
 package com.sicredi.ornitologosbackend.services;
 
+import com.sicredi.ornitologosbackend.entities.Ave;
+import com.sicredi.ornitologosbackend.repositories.AveRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
 public class AveService {
+
+    private final AveRepository aveRepository;
+    public Ave inserirAve(Ave ave){
+        return aveRepository.save(ave);
+    }
+
+    public List<Ave> encontrarAve(String busca){
+        return aveRepository
+                .findByNomePtContainingOrNomeEnContainingOrNomeLtContainingOrCorOrHabitat
+                        (busca, busca, busca, busca, busca);
+    }
 }
