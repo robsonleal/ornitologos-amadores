11a9cb5fbf87264430adb6843a2b3b68be03d957
kayque <kayque_silva@sicredi.com.br>
2022-11-03 11:08:44 -0300

    adiciona dependencia h2 e testa usuarioRepository

--- ornitologos-backend/src/main/java/com/sicredi/ornitologosbackend/entities/Usuario.java
+++ ornitologos-backend/src/main/java/com/sicredi/ornitologosbackend/entities/Usuario.java
@@ -6,6 +6,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
@@ -14,6 +17,8 @@ import javax.persistence.Entity;
 @Builder
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String nome;
     private String email;
