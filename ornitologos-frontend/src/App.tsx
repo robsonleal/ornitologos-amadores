9a44f3163eeed2f750528b3e223addc5e8377e95
Robson Leal <robson_leal@outlook.com.br>
2022-11-07 22:36:06 -0300

    Instalando e configurando as primeiras rotas para teste

--- ornitologos-frontend/src/App.tsx
+++ ornitologos-frontend/src/App.tsx
@@ -1,34 +1,11 @@
-import { Grid, CssBaseline, Container } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter } from "react-router-dom";
 
-import { Topbar } from "./shared/components/Topbar";
-import { Banner } from "./shared/components/Banner";
-import { Footer } from "./shared/components/Footer";
-import { GridCard } from "./shared/components/GridCard";
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-const theme = createTheme();
+import { AppRoutes } from "./routes";
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Topbar position="relative" />
-      <main>
-        <Banner />
-
-        <Container sx={{ py: 8 }} maxWidth="md">
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <GridCard value={card} />
-            ))}
-          </Grid>
-        </Container>
-      </main>
-      {/* Footer */}
-      <Footer />
-      {/* End footer */}
-    </ThemeProvider>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
---
+++ ornitologos-frontend/src/Teste.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@mui/material";
+
+export function Teste() {
+  return <Button variant="contained">Hello world!</Button>;
+}
