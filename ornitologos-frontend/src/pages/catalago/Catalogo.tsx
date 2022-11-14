5e08417ace8df311548776a25582d12628edf254
Robson Leal <robson_leal@outlook.com.br>
2022-11-14 19:44:45 -0300

    Frontend: Criando um hook personalizado para evitar consultas desnecessárias ao backend, durante preenchimento do campo de pesquisa

--- ornitologos-frontend/src/pages/catalago/Catalogo.tsx
+++ ornitologos-frontend/src/pages/catalago/Catalogo.tsx
@@ -1,23 +1,27 @@
 import { useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { BarraDeFerramentas } from '../../shared/components';
+import { useDebounce } from '../../shared/hooks';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import { AvesService } from '../../shared/services/api/aves/AvesService';
 
 export const Catalago = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce();
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(() => {
-    AvesService.getAll(1, busca).then((result) => {
-      if (result instanceof Error) {
-        alert(result.message);
-      } else {
-        console.log(result);
-      }
+    debounce(() => {
+      AvesService.getAll(1, busca).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          console.log(result);
+        }
+      });
     });
   }, [busca]);
 
