0fb2868d1ed712da0ebc7f574d92dc025d8d64f7
Robson Leal <robson_leal@outlook.com.br>
2022-11-14 18:24:03 -0300

    Frontend: Usando search params para caixa de pesquisa do catalogo de aves

--- ornitologos-frontend/src/pages/catalago/Catalogo.tsx
+++ ornitologos-frontend/src/pages/catalago/Catalogo.tsx
@@ -1,11 +1,26 @@
+import { useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { BarraDeFerramentas } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 
 export const Catalago = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const busca = useMemo(() => {
+    return searchParams.get('busca') || '';
+  }, [searchParams]);
+
   return (
     <LayoutBaseDePagina
       titulo='Catálogo'
-      barraDeFerramentas={<BarraDeFerramentas />}
+      barraDeFerramentas={
+        <BarraDeFerramentas
+          textoDaBusca={busca}
+          aoMudarTextoDeBusca={(texto) =>
+            setSearchParams({ busca: texto }, { replace: true })
+          }
+        />
+      }
     >
       Aqui vai os cards de aves
     </LayoutBaseDePagina>
